1b072cda03789d9d469931c64c20193430184456
Sue-Hyeong Lee <sue309667@gmail.com>
2026-06-29 17:03:51 +0900

    1-5, 1-6, 1-7

---
+++ 1/1-7/1-7.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Comp.h"
+
+int main(void) {
+
+	BestComImpl::SimpleFunc();
+	ProgComImpl::SimpleFunc();
+}
---
+++ 1/1-7/Comp.h
@@ -0,0 +1,20 @@
+#ifndef __MY_HEADER__
+#define __MY_HEADER__
+
+namespace BestComImpl
+{
+	void SimpleFunc(void);
+}
+
+namespace BestComImpl
+{
+	void PrettyFunc(void);
+}
+
+
+namespace ProgComImpl
+{
+	void SimpleFunc(void);
+}
+
+#endif

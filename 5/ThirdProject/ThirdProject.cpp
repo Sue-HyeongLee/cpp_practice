c2cd4b7b442d0a570f66b57f473c56beab1cf564
Sue-Hyeong Lee <hermi6143@naver.com>
2026-07-03 23:42:03 +0900

    복사 생성자 heap 방식에서 문제 생길 수 있으므로 사용! 복사 생성자의 활용... heap하고 그냥 복사시 delete시 같은 곳 삭제해서 오류 발생.

---
+++ 5/ThirdProject/ThirdProject.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include "Bank.h"
+#include <stdlib.h>
+using namespace std;
+
+/*
+ 
+ * 1단계
+ 통장의 계좌번호는 중복되지 아니한다.
+ 입금 및 출금액은 무조건 0보다 크다.
+ 고객의 계좌 정보는 계좌번호, 고객 이름, 고객의 잔액 세 가지만 저장 및 관리한다.
+ 둘 이상의 고객 정보 저장을 위해서 배열을 사용한다.
+ 계좌번호는 정수의 형태이다.
+
+ * 2단계
+ class를 만들어보자. 캡슐화와 은닉화는 어떻게? 생성자와 소멸자는 어떻게?
+ version 0.1의 Account 구조체는 char형 배열을 뒀지만 동적 할당으로 생성해보자.(문자열 포인터)
+ 또한 객체를 저장하는 배열을 포인터로 만들어보자! 
+
+ * 3단계
+ 복사 생성자를 활용하진 않지만 heap 영역을 사용할 경우, 얕은 복사는 문제가 될 수 있다. 따라서
+ 따라서 우리는 깊은 복사를 해야한다.
+ 객체 생성자할 떄 &를 붙여야 하는 이유는 객체 생성자 호출 시점 때문이다.
+ 1. int num = num과 같이 대입할 때
+ 2. 인자에 넣을 때 넣어지면서 객체 초기화된다.
+ 3. return할 때 임시변수/임시객체로 나와진다.
+ 2 번째 경우로 그래서 무한 굴레에 빠지기 떄문에 &를 무조건 붙여야한다.
+*/
+
+
+int main(void) {
+
+	Bank* bank = new Bank;
+	
+	
+	int select = 0;
+	while (select != 5) {
+		cout << "-----Menu------" << endl;
+		cout << "1. 계좌 개설" << endl;
+		cout << "2. 입금" << endl;
+		cout << "3. 출금" << endl;
+		cout << "4. 계좌정보 전체 출력" << endl;
+		cout << "5. 프로그램 종료" << endl;
+		cout << "선택: ";
+
+		cin >> select;
+
+		cout << endl << endl;
+
+
+		switch (select) {
+		case MAKE:
+			bank->MakeAccount();
+			break;
+		case DEPOSIT:
+			bank->DepositMoney();
+			break;
+		case WITHDRAW:
+			bank->WithDrawMoney();
+			break;
+		case INQUIRE:
+			bank->ShowAllAccInfo();
+			break;
+		case EXIT:
+			return 0;
+		default:
+			cout << "Illegal selection.." << endl;
+		}
+		
+
+		cout << endl << endl;
+	
+	}
+
+	delete bank;
+	return 0;
+	
+}

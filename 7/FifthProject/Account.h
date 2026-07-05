dbd0d8b8865b28f350ade0a0056335f87059ec37
Sue-Hyeong Lee <hermi6143@naver.com>
2026-07-05 21:58:22 +0900

    FifthProject 추가 딱히 추가할 건 없었으나, Showmenu는 Bank에 넣는게 맞다 생각해서 넣음.

---
+++ 7/FifthProject/Account.h
@@ -0,0 +1,29 @@
+#ifndef __BANK_H__ 
+#define __BANK_H__
+
+class Account {
+	int id;
+	char* name;
+	int money;
+
+public:
+	Account(int id, char* name, int money);
+
+	Account(const Account& account);
+
+	void DepositMoney(int money);
+
+	void WithDrawMoney(int money);
+
+	void ShowAllInfo(void) const;
+
+	int GetID(void) const;
+
+	char* GetName(void) const;
+
+	int GetMoney(void) const;
+
+	~Account();
+
+};
+#endif
